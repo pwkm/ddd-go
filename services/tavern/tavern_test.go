72b4b76c0cedf89bfa3063988f4cd6b242aac971
Peter Winkelmans <peter.winkelmans@sennit.be>
2023-07-20 09:48:06 +0200

    Final

--- services/tavern/tavern_test.go
+++ services/tavern/tavern_test.go
@@ -4,18 +4,37 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
-	"github.com/pwkm/tavern/domain/customer"
+	"github.com/pwkm/tavern/domain/product"
 	"github.com/pwkm/tavern/services/order"
 )
+
+func init_products(t *testing.T) []product.Product {
+	beer, err := product.NewProduct("Beer", "Healthy Beverage", 1.99)
+	if err != nil {
+		t.Error(err)
+	}
+	peenuts, err := product.NewProduct("Peenuts", "Healthy Snacks", 0.99)
+	if err != nil {
+		t.Error(err)
+	}
+	wine, err := product.NewProduct("Wine", "Healthy Snacks", 0.99)
+	if err != nil {
+		t.Error(err)
+	}
+	products := []product.Product{
+		beer, peenuts, wine,
+	}
+	return products
+}
 
 func Test_Tavern(t *testing.T) {
 	// Create OrderService
 
-	products := order.init_products(t)
+	products := init_products(t)
 
 	os, err := order.NewOrderService(
-		// WithMemoryCustomerRepository(),
-		order.WithMongoCustomerRepository("mongodb://localhost:2717"),
+		order.WithMemoryCustomerRepository(),
+		//order.WithMongoCustomerRepository("mongodb://localhost:2717"),
 		order.WithMemoryProductRepository(products),
 	)
 	if err != nil {
@@ -27,12 +46,7 @@ func Test_Tavern(t *testing.T) {
 		t.Error(err)
 	}
 
-	cust, err := customer.NewCustomer("Percy")
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = os.customers.Add(cust)
+	uid, err := os.AddCustomer("percy")
 	if err != nil {
 		t.Error(err)
 	}
@@ -40,7 +54,7 @@ func Test_Tavern(t *testing.T) {
 		products[0].GetID(),
 	}
 	// Execute Order
-	err = tavern.Order(cust.GetID(), order)
+	err = tavern.Order(uid, order)
 	if err != nil {
 		t.Error(err)
 	}
