5007171af11a69216d9b1142933d735d3d18e70e
Peter Winkelmans <peter.winkelmans@sennit.be>
2024-06-14 18:28:57 +0200

    second commit

--- aggregate/customer.go
+++ aggregate/customer.go
@@ -2,8 +2,16 @@
 package aggregate
 
 import (
+	"errors"
+
+	"github.com/google/uuid"
 	"github.com/pwkm/ddd-go/entity"
 	"github.com/pwkm/ddd-go/valueobject"
+)
+
+var (
+	// ErrInvalidPerson is returned when the person is not valid in the NewCustome factory
+	ErrInvalidPerson = errors.New("a customer has to have an valid person")
 )
 
 // Customer is a aggregate that combines all entities needed to represent a customer
@@ -15,4 +23,25 @@ type Customer struct {
 	products []*entity.Item
 	// a customer can perform many transactions
 	transactions []valueobject.Transaction
+}
+
+// NewCustomer is a factory to create a new Customer aggregate
+// It will validate that the name is not empty
+func NewCustomer(name string) (Customer, error) {
+	// Validate that the Name is not empty
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	// Create a new person and generate ID
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+	// Create a customer object and initialize all the values to avoid nil pointer exceptions
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
 }
