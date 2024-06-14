f627a744d030f32abce0a4b12e71a6856fb02df9
Peter Winkelmans <peter.winkelmans@sennit.be>
2024-06-14 19:03:29 +0200

    step 5

---
+++ memory/memory.go
@@ -0,0 +1,37 @@
+// Package memory is a in-memory implementation of the customer repository
+package memory
+
+import (
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/pwkm/ddd-go/aggregate"
+)
+
+// MemoryRepository fulfills the CustomerRepository interface
+type MemoryRepository struct {
+	customers map[uuid.UUID]aggregate.Customer
+	sync.Mutex
+}
+
+// New is a factory function to generate a new repository of customers
+func New() *MemoryRepository {
+	return &MemoryRepository{
+		customers: make(map[uuid.UUID]aggregate.Customer),
+	}
+}
+
+// Get finds a customer by ID
+func (mr *MemoryRepository) Get(uuid.UUID) (aggregate.Customer, error) {
+	return aggregate.Customer{}, nil
+}
+
+// Add will add a new customer to the repository
+func (mr *MemoryRepository) Add(aggregate.Customer) error {
+	return nil
+}
+
+// Update will replace an existing customer information with the new customer information
+func (mr *MemoryRepository) Update(aggregate.Customer) error {
+	return nil
+}
